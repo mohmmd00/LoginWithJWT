c31a086bdd6c8126ef66693ede89f12c6645219b
mohammadhosein joshaghani <mohammadh.josh@proton.me>
2025-03-04 09:07:29 +0330

    added application services such as authservice and passwordService

---
+++ TokenAuth/TA.Application/Services/AuthService.cs
@@ -0,0 +1,60 @@
+﻿using TA.Application.DTO_s;
+using TA.Domain.Entities;
+using TA.Domain.Interfaces;
+
+namespace TA.Application.Services
+{
+    public class AuthService
+    {
+        private readonly ISessionRepository _sessionRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordService _passwordService;
+        private readonly IJwtService _jwtService;
+        public AuthService(ISessionRepository sessionRepository, IUserRepository userRepository, IPasswordService passwordService, IJwtService jwtService)
+        {
+            _sessionRepository = sessionRepository;
+            _userRepository = userRepository;
+            _passwordService = passwordService;
+            _jwtService = jwtService;
+        }
+
+        public async Task Register(RegisterRequest request)
+        {
+            var existingUser = _userRepository.GetUserby(request.Username);
+            if (existingUser != null)
+            {
+                throw new Exception("Username already exists.");
+            }
+
+            string salt = Guid.NewGuid().ToString();
+            string passwordHash = _passwordService.HashedPassword(request.Password, salt);
+
+            var user = new User(request.Username, passwordHash, salt);
+
+
+
+            //make sure that user saves in db make sure repository does have a method to save newly created user !!!!!!!!!!!!!!
+
+        }
+        public async Task<LoginResponse> Login(LoginRequest request)
+        {
+            var fetchedUser = _userRepository.GetUserby(request.Username);
+            if (fetchedUser == null)
+                throw new Exception("Invalid username or password");
+            bool isPasswordValid = _passwordService.VerifiedPassword(request.Password, fetchedUser.Salt, fetchedUser.Password);
+            if (!isPasswordValid)
+                throw new Exception("Invalid username or password");
+
+            string sessionId = Guid.NewGuid().ToString();
+
+            var newSession = new Session(fetchedUser.PrimaryId, sessionId);
+
+            _sessionRepository.SaveChanges(newSession);
+
+            string token = _jwtService.GenerateToken(fetchedUser.PrimaryId, sessionId);
+
+
+            return new LoginResponse { Token = token };
+        }
+    }
+}
---
+++ TokenAuth/TA.Application/Services/PasswordService.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography; 
+using System.Text;
+using TA.Domain.Interfaces;
+
+namespace TA.Application.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public string HashedPassword(string password, string salt)
+        {
+            if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(salt))
+                throw new ArgumentNullException("Password or salt cannot be null or empty.");
+            
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] saltBytes = Encoding.UTF8.GetBytes(salt);
+
+            using (var hmac = new HMACSHA256(saltBytes))
+            {
+                byte[] hashBytes = hmac.ComputeHash(passwordBytes);
+
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
+        public bool VerifiedPassword(string password, string salt, string hash)
+        {
+            if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(salt) || string.IsNullOrEmpty(hash))
+                throw new ArgumentNullException("Password, salt, or hash cannot be null or empty.");
+
+
+            string computedHash = HashedPassword(password, salt);
+
+            return computedHash == hash;
+        }
+    }
+}
