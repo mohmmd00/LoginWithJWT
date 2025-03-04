d619f72ad0c6b09574545b02eb183fe27e4dee12
mohammadhosein joshaghani <mohammadh.josh@proton.me>
2025-03-04 09:04:19 +0330

    added domain layer entities and domaion project

---
+++ TokenAuth/TA.Domain/Entities/Session.cs
@@ -0,0 +1,14 @@
+﻿namespace TA.Domain.Entities
+{
+    public class Session
+    {
+        public int UserId { get; set; }
+        public string SessionId { get; set; }
+
+        public Session(int userId, string sessionId)
+        {
+            UserId = userId;
+            SessionId = sessionId;
+        }
+    }
+}
---
+++ TokenAuth/TA.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace TA.Domain.Entities
+{
+    public class User
+    {
+        public int PrimaryId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+
+        public User(string username, string password, string salt)
+        {
+            Username = username;
+            Password = password;
+            Salt = salt;
+        }
+    }
+}
