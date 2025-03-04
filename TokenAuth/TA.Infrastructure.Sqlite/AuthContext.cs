0a409928270ee8bc24a9842f8d0dc865910ba0a9
mohammadhosein joshaghani <mohammadh.josh@proton.me>
2025-03-04 13:21:32 +0330

    added infrastructure layer and wrote context an created repository for user 'ididnt wrote anything to it'

---
+++ TokenAuth/TA.Infrastructure.Sqlite/AuthContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TA.Domain.Entities;
+
+namespace TA.Infrastructure.Sqlite
+{
+    public class AuthContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public AuthContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(key => key.PrimaryId);
+            modelBuilder.Entity<Session>().HasKey(key => key.UserId);
+
+
+            modelBuilder.Entity<Session>().HasOne<User>().WithOne().HasForeignKey<Session>(x => x.UserId);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ TokenAuth/TA.Infrastructure.Sqlite/Repository/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using TA.Domain.Entities;
+using TA.Domain.Interfaces;
+
+namespace TA.Infrastructure.Sqlite.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public User GetUserby(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUserExistsBy(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveNewUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
